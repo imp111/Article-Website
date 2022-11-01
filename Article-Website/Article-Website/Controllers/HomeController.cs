67b4286f9f144d8e31129f8562af8f922742bc9e
imp111 <alexsandartenev@gmail.com>
2022-11-02 00:29:01 +0200

    register and login page added

--- Article-Website/Article-Website/Controllers/HomeController.cs
+++ Article-Website/Article-Website/Controllers/HomeController.cs
@@ -23,6 +23,16 @@ namespace Article_Website.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
