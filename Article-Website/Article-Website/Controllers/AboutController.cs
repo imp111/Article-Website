1ee3e41452e5c474100d2c15a70d8c6f066cba79
imp111 <alexsandartenev@gmail.com>
2023-01-28 18:31:33 +0200

    about view and controller added

---
+++ Article-Website/Article-Website/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Article_Website.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
