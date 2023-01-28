b99694e08560857489b6e5e7ce1f24b3a5948897
imp111 <alexsandartenev@gmail.com>
2023-01-28 19:19:50 +0200

    users model added

---
+++ Article-Website/Article-Website/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Article_Website.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Article_Website.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Users> Users { get; set; }
+    }
+}
---
+++ Article-Website/Article-Website/Models/Users.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Article_Website.Models
+{
+    public class Users
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Email { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+        
+    }
+}
